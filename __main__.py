5c92b53d422d1fe79bba60691ee74685ea260ecc
thopp-tudelft <t.r.hoppener@tudelft.nl>
2023-05-01 16:52:32 +0200

    setup basic logging to file

--- __main__.py
+++ __main__.py
@@ -1,3 +1,27 @@
-from . import Fiumicino
+import logging
+import pathlib
 
-Fiumicino.run_app()
+# set up logging first
+logfile = pathlib.Path(f"./{__package__}.log")
+try:
+    logfile.rename(f"{logfile}.previous")
+except FileNotFoundError:
+    pass
+
+file_handler = logging.FileHandler(
+    filename=logfile,
+)
+logging.basicConfig(
+    handlers=[file_handler, logging.StreamHandler()],
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+)
+
+
+def run():
+    from . import Fiumicino  # TODO fix modules misbehaving on import
+
+    Fiumicino.run_app()
+
+
+run()
